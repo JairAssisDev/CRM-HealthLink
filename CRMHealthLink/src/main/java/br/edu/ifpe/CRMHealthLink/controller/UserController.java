3cf3af3659013d6702451b02f193df604fbef8f6
JairAssisDev <jairv141@gmail.com>
2024-07-07 15:55:20 -0300

    [CREATE] -CRUD user

--- CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/controller/UserController.java
+++ CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/controller/UserController.java
@@ -1,6 +1,7 @@
 package br.edu.ifpe.CRMHealthLink.controller;
 
 import br.edu.ifpe.CRMHealthLink.dto.UserCreateDto;
+import br.edu.ifpe.CRMHealthLink.dto.UserResponseDto;
 import br.edu.ifpe.CRMHealthLink.dto.mapper.UserMapper;
 import br.edu.ifpe.CRMHealthLink.entity.User;
 import br.edu.ifpe.CRMHealthLink.service.UserService;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -25,4 +28,42 @@ public class UserController {
         User responseUser = userService.save(UserMapper.toUserItem(user));
         return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
+
+    @Operation(summary = "Retorna todos os usuarios",description = "...")
+    @GetMapping
+    private ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.getAllUser();
+        return ResponseEntity.ok(users);
+    }
+
+    @Operation(summary = "Retorna o usuario pelo id",description = "...")
+    @GetMapping("/{id}")
+    private ResponseEntity<UserResponseDto> getUserById(@RequestParam Long id) {
+        User user = userService.findById(id);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(UserMapper.toDtoUser(user));
+
+    }
+
+    @Operation(summary = "Remove o usuario pelo id",description = "...")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(@PathVariable Long id) {
+        try {
+            userService.delete(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(long id , UserCreateDto user) {
+        userService.update(id,user);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
 }
+
--- CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/service/UserService.java
+++ CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/service/UserService.java
@@ -1,13 +1,19 @@
 package br.edu.ifpe.CRMHealthLink.service;
 
-import br.edu.ifpe.CRMHealthLink.entity.User;
-import br.edu.ifpe.CRMHealthLink.repository.UserRepository;
-import br.edu.ifpe.CRMHealthLink.exception.ResourceNotFoundException;
-import lombok.RequiredArgsConstructor;
 
+import br.edu.ifpe.CRMHealthLink.dto.UserCreateDto;
+import br.edu.ifpe.CRMHealthLink.dto.mapper.UserMapper;
+import br.edu.ifpe.CRMHealthLink.entity.AcessLevel;
+import br.edu.ifpe.CRMHealthLink.entity.User;
+import br.edu.ifpe.CRMHealthLink.exception.ResourceNotFoundException;
+import br.edu.ifpe.CRMHealthLink.repository.UserRepository;
+import jakarta.validation.constraints.Size;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -22,20 +28,35 @@ public class UserService {
     }
 
     @Transactional(readOnly = true)
-    public List<User> getAllTask() {
+    public List<User> getAllUser() {
         return userRepository.findAll();
     }
 
     @Transactional
-    public User update(User user) {
-        return userRepository.save(user);
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+    @Transactional
+    public void delete(Long id) {
+       User user = userRepository.findById(id).orElseThrow(
+               () -> new ResourceNotFoundException("Usuairo não encontrado.")
+               );
+       userRepository.delete(user);
     }
 
     @Transactional
-    public void delete(long id) {
+    public User update(long id, UserCreateDto newUser) {
         User user = userRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("Usuarios não encontrado.")
+                () -> new ResourceNotFoundException("Usuairo não encontrado.")
         );
-        userRepository.delete(user);
+        user.setName(newUser.getName());
+        user.setName(newUser.getLogin());
+        user.setCpf(newUser.getCpf());
+        user.setLevel(newUser.getLevel());
+        user.setDayOfBirth(newUser.getDayOfBirth());
+        user.setPassword(newUser.getPassword());
+
+        return userRepository.save(user);
+
     }
 }
