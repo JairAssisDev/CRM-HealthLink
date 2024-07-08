d4a5e51674c0bf572c29876c4e6fb6fc123a8c6a
Matheus Kennedy <40745186+MasterKingRR@users.noreply.github.com>
2024-07-08 09:02:50 -0300

    fix

--- CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/entity/AcessLevel.java
+++ CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/entity/AcessLevel.java
@@ -1,7 +1,7 @@
 package br.edu.ifpe.CRMHealthLink.entity;
 
 public enum AcessLevel {
-    PATIENT(1),DOCTOR(2),ATTENDANT(3),MANGER(4);
+    PATIENT(1),DOCTOR(2),ATTENDANT(3),MANAGER(4);
     public final Integer level;
     AcessLevel(Integer level){
         this.level = level;
