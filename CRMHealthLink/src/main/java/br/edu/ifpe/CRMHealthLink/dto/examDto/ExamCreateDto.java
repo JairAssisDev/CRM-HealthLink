21d7e7babbca588812c3de8a61b61260c631d813
JairAssisDev <jairv141@gmail.com>
2024-07-17 11:32:27 -0300

    Create - service ,Repository ,Dto Mapper  from Exam fix -  add @AllArgsConstructor

---
+++ CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/dto/examDto/ExamCreateDto.java
@@ -0,0 +1,12 @@
+package br.edu.ifpe.CRMHealthLink.dto.examDto;
+
+import java.time.LocalDateTime;
+
+public class exemCreateDto {
+
+    private LocalDateTime date;
+
+    private String descricao;
+
+    private Long fk_appointment;
+}
---
+++ CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/dto/examDto/ExamResponseDto.java
@@ -0,0 +1,11 @@
+package br.edu.ifpe.CRMHealthLink.dto.examDto;
+
+import java.time.LocalDateTime;
+
+public class examResponseDto {
+    private LocalDateTime date;
+    private String description;
+    private String namePatient;
+    private String nameDoctor;
+    private String descriptionAppointment;
+}
