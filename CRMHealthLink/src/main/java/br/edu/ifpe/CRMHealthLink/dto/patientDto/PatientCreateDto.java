8b036b7fe24151739ebc8c33bc80a731f34a2881
Matheus Kennedy <40745186+MasterKingRR@users.noreply.github.com>
2024-07-20 09:50:21 -0300

    Add files via upload

--- CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/dto/patientDto/PatientCreateDto.java
+++ CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/dto/patientDto/PatientCreateDto.java
@@ -1,11 +1,23 @@
 package br.edu.ifpe.CRMHealthLink.dto.patientDto;
 
 import br.edu.ifpe.CRMHealthLink.dto.baseUserDto.UserCreateDto;
+import br.edu.ifpe.CRMHealthLink.entity.AcessLevel;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-
+import java.time.LocalDate;
 
 
 public class PatientCreateDto extends UserCreateDto {
+    public PatientCreateDto(String name, LocalDate date, String cpf, String email, AcessLevel acessLevel, String password) {
+        super(name, date, cpf, email, acessLevel, password);
+    }
+    public PatientCreateDto(){
 
+    }
 }
+
