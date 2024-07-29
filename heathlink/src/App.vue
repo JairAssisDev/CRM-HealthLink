10712d7b32ae852debcf13e79009a821173bf517
Lucas <luvasmanoel14@gmail.com>
2024-07-28 23:40:08 -0300

    Adicionando setup inicial Vue.js

---
+++ heathlink/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ heathlink/src/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <!-- desktop header -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Doctor</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Departments</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Search</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">LOGIN</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+/* Adicione aqui o CSS do seu cabeçalho */
+</style>
