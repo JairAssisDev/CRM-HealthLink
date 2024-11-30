e67fed5b2b2a772c3520919223867ca12e75273d
Matheus Kennedy <matheus2004santiago@gmail.com>
2024-11-30 11:35:03 -0300

    sdp

---
+++ CRMHealthLink/src/main/java/br/edu/ifpe/CRMHealthLink/infra/sdp/AuthHandshakeHandler.java
@@ -0,0 +1,38 @@
+package br.edu.ifpe.CRMHealthLink.infra.sdp;
+
+import br.edu.ifpe.CRMHealthLink.infra.security.TokenService;
+import br.edu.ifpe.CRMHealthLink.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
+
+@Component
+public class AuthHandshakeHandler extends DefaultHandshakeHandler {
+    private IUserRepository userRepo;
+
+    public AuthHandshakeHandler(IUserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    protected Principal determineUser(ServerHttpRequest request,
+                                      WebSocketHandler wsHandler,
+                                      Map<String, Object> attributes) {
+
+
+        String username = (String) attributes.get("username");
+        UserDetails user = userRepo.findByUsername(username);
+        var auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        return auth;
+    }
+}
