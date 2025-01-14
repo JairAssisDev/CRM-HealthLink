b0d3172f315984a0da55d56d60b4863f1fe96bef
JairAssisDev <jairv141@gmail.com>
2025-01-13 23:28:53 -0300

    Add: AppointmentService

---
+++ CRMHealthLink/src/test/java/br/edu/ifpe/CRMHealthLink/service/AppointmentServiceTest.java
@@ -0,0 +1,48 @@
+package br.edu.ifpe.CRMHealthLink.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getByDoctorAndPatientAndDateAndInicio() {
+    }
+
+    @Test
+    void getApByDoctorAndPatientAndDateAndInicio() {
+    }
+
+    @Test
+    void getAllAppointment() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void criar() {
+    }
+
+    @Test
+    void consultasPaciente() {
+    }
+
+    @Test
+    void consultasMedicoCrm() {
+    }
+}
