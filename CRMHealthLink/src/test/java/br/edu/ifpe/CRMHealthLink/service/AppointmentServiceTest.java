6497efeba34b084dcfd7e2b7d909a90283e3f6b1
JairAssisDev <jairv141@gmail.com>
2025-01-13 23:29:24 -0300

    Add: AppointmentServerce

--- CRMHealthLink/src/test/java/br/edu/ifpe/CRMHealthLink/service/AppointmentServiceTest.java
+++ CRMHealthLink/src/test/java/br/edu/ifpe/CRMHealthLink/service/AppointmentServiceTest.java
@@ -7,42 +7,84 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppointmentServiceTest {
 
     @Test
-    void save() {
-    }
-
-    @Test
     void getByDoctorAndPatientAndDateAndInicio() {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     void getApByDoctorAndPatientAndDateAndInicio() {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     void getAllAppointment() {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     void findById() {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     void delete() {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     void update() {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     void criar() {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     void consultasPaciente() {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     void consultasMedicoCrm() {
+        // Arrange
+
+        // Act
+
+        // Assert
+
     }
 }
